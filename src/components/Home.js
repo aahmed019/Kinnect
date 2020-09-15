12760fe8e3a1d3c3f7b7148e936d8c1270b3cbca
Lam Tran <ltran96@protonmail.com>
2020-09-14 21:38:14 -0400

    Add Camera Component inside functionalities

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,6 +1,7 @@
 import React, { useState } from 'react';
 import Game from './game';
 import About from './about';
+import Functionalities from './functionalities';
 import { StyleSheet, Text, View, Alert, Image, ImageBackground } from 'react-native';
 import Lobby from './lobby';
 import Contact from './contact';
@@ -17,6 +18,7 @@ export default function App() {
   const [aboutPage, setAboutPage] = useState(false)
   const [contactPage, setContactPage] = useState(false)
   const [showAds, setShowAds] = useState(true)
+  const [testingPage, setTestingPage] = useState(false)
   if (newRoomPage) {
     return (<Game style={{ alignItems: 'center' }} />)
   } else if (joinRoomPage) {
@@ -25,63 +27,76 @@ export default function App() {
     return (<Contact style={{ alignItems: 'center' }} />)
   } else if (aboutPage) {
     return (<About style={{ alignItems: 'center' }} />)
+  } else if (testingPage) {
+    return (<Functionalities style={{ alignItems: 'center' }} />)
   }
   else {
     return (
       <View style={styles.container}>
-          <Text>{"\n"}{"\n"}{"\n"}</Text>
-          {showAds
-            ? <AdMobBanner
-              style={styles.adContainer}
-              bannerSize="fullBanner"
-              adUnitID="ca-app-pub-3940256099942544/2934735716" // Test ID, Replace with your-admob-unit-id
-              testDeviceID="EMULATOR"
-              servePersonalizedAds // true or false
-              onDidFailToReceiveAdWithError={(e) => { console.log(e) }} />
-            : console.log("no ads")
-          }
-          <Text style={styles.title}>Kinnect</Text>
-          <Text>{"\n"}</Text>
+        <Text>{"\n"}{"\n"}{"\n"}</Text>
+        {showAds
+          ? <AdMobBanner
+            style={styles.adContainer}
+            bannerSize="fullBanner"
+            adUnitID="ca-app-pub-3940256099942544/2934735716" // Test ID, Replace with your-admob-unit-id
+            testDeviceID="EMULATOR"
+            servePersonalizedAds // true or false
+            onDidFailToReceiveAdWithError={(e) => { console.log(e) }} />
+          : console.log("no ads")
+        }
+        <Text style={styles.title}>Kinnect</Text>
+        <Text>{"\n"}</Text>
 
-          <Text style={styles.items} textStyle={styles.items} onPress={() => {
-            setNewRoom(true)
-            setJoinRoom(false)
-            setAboutPage(false)
-            setContactPage(false)
-          }}>Create New Room</Text>
-          
-          <Text style={styles.items} textStyle={styles.items} onPress={() => {
-            setNewRoom(false)
-            setJoinRoom(true)
-            setAboutPage(false)
-            setContactPage(false)
-          }}>Join A Room</Text>
-          
-          <Text style={styles.items} onPress={() => {
-            setNewRoom(false)
-            setAboutPage(true)
-            setJoinRoom(false)
-            setContactPage(false)
-          }}>About Us</Text>
-          
-          <Text style={styles.items} onPress={() => {
-            setNewRoom(false)
-            setAboutPage(false)
-            setJoinRoom(false)
-            setContactPage(true)
-          }}>Contact Us</Text>
-          <Text>{'\n'}{'\n'}{'\n'}{'\n'}{'\n'}{'\n'}{'\n'}</Text>
-          {
-            showAds
-              ? <Text style={{ position: 'absolute', bottom: 60 }} onPress={() => {
-                setShowAds(false)
-                Alert.alert('Thank You for Your Donation')
-              }}>Remove Ads $0.99</Text>
-              : <Text style={{ position: 'absolute', bottom: 60 }} onPress={() => {
-                setShowAds(true)
-                Alert.alert('Please give me your money to remove ad')
-              }}>Restore Purchase</Text>
-          }
+        <Text style={styles.items} textStyle={styles.items} onPress={() => {
+          setNewRoom(true)
+          setJoinRoom(false)
+          setAboutPage(false)
+          setContactPage(false)
+          setTestingPage(false)
+        }}>Create New Room</Text>
+
+        <Text style={styles.items} textStyle={styles.items} onPress={() => {
+          setNewRoom(false)
+          setJoinRoom(true)
+          setAboutPage(false)
+          setContactPage(false)
+          setTestingPage(false)
+        }}>Join A Room</Text>
+
+        <Text style={styles.items} onPress={() => {
+          setNewRoom(false)
+          setAboutPage(true)
+          setJoinRoom(false)
+          setContactPage(false)
+          setTestingPage(false)
+        }}>About Us</Text>
+
+        <Text style={styles.items} onPress={() => {
+          setNewRoom(false)
+          setAboutPage(false)
+          setJoinRoom(false)
+          setContactPage(true)
+          setTestingPage(false)
+        }}>Contact Us</Text>
+        <Text style={styles.items} onPress={() => {
+          setNewRoom(false)
+          setAboutPage(false)
+          setJoinRoom(false)
+          setContactPage(false)
+          setTestingPage(true)
+        }}>Functionalities</Text>
+        <Text>{'\n'}{'\n'}{'\n'}{'\n'}{'\n'}{'\n'}{'\n'}</Text>
+        {
+          showAds
+            ? <Text style={{ position: 'absolute', bottom: 60 }} onPress={() => {
+              setShowAds(false)
+              Alert.alert('Thank You for Your Donation')
+            }}>Remove Ads $0.99</Text>
+            : <Text style={{ position: 'absolute', bottom: 60 }} onPress={() => {
+              setShowAds(true)
+              Alert.alert('Please give me your money to remove ad')
+            }}>Restore Purchase</Text>
+        }
       </View>
     )
   }
