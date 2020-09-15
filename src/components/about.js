9a4a7b0d3b07d9e94c8a0de97ad1135d662938ba
aqsa505 <aqsamalik70@gmail.com>
2020-09-14 20:56:44 -0400

    update page

--- src/components/about.js
+++ src/components/about.js
@@ -11,17 +11,13 @@ const Support = (props) => {
       {
         !back
           ? <View style={styles.container}>
-            
+             <Text>{"\n"}</Text>
+            <TouchableOpacity  style={styles.button}><Text style ={styles.buttontext}>Back</Text></TouchableOpacity>
             <Text style={styles.title}>About Us</Text>
             <Text>{"\n"}</Text>
             <Text style={styles.text}>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed in aliquam neque. Proin quis hendrerit lorem, vel molestie sapien. Vivamus blandit gravida lorem, id ultrices urna elementum vitae. Nunc rhoncus finibus pellentesque. Quisque eu auctor justo. Proin ultrices, lorem sit amet commodo finibus, turpis odio commodo orci, eu accumsan diam erat a turpis. Maecenas eu feugiat enim, id ultricies velit. Maecenas lobortis velit a cursus auctor. In luctus rhoncus purus eu auctor. Integer aliquam nibh sit amet aliquam suscipit. Vivamus dignissim eros non orci rhoncus, eu ornare urna tincidunt. Nunc et ex gravida, imperdiet odio nec, mollis sem. Donec convallis ullamcorper felis id tempor. Curabitur non ipsum dapibus, tempor elit et, iaculis ex. Pellentesque blandit egestas dui, sed placerat.
              {"\n"}Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed in aliquam neque. Proin quis hendrerit lorem,</Text>
-             <TouchableOpacity
-        style={styles.button}
-        onPress={() => setBack(true)}   
-      >
-        <Text style={styles.buttontext}>back</Text>
-      </TouchableOpacity>
+             
       <Text>{"\n"}</Text>
           </View>
           : <Home />
@@ -32,31 +28,33 @@ const Support = (props) => {
 
 const styles = StyleSheet.create({
   container: {
-    backgroundColor: 'white',
+    backgroundColor: '#2b2d40',
     alignItems: 'center',
-    paddingTop: '10%',
+    paddingTop: 30,
+    paddingLeft : 25,
+    paddingRight : 25,
     height: '100%',
     width: '100%',
     justifyContent: 'center'
   },
   title: {
     fontFamily: 'GillSans-Bold',
-    fontSize: 60,
+    fontSize: 50,
     paddingTop : 20,
    
     textAlign: 'center',
-    color: 'black',
+    color: 'white',
 
   },
   button: {
     alignItems: "center",
-    backgroundColor: "red",
+    backgroundColor: "white",
     color : "white",
     paddingLeft : 20,
     paddingRight: 20,
     paddingTop : 10,
     paddingBottom :10,
-   
+    alignSelf : 'flex-start',
     borderRadius:10,
     borderWidth: 1,
     borderColor: '#fff',
@@ -66,11 +64,10 @@ const styles = StyleSheet.create({
     color :'white',
   },
   text : {
-    color : 'red',
+    color : 'white',
     fontFamily : 'Gill Sans', 
     fontSize : 20,
-    paddingLeft : 25,
-    paddingRight : 25,
+    
     paddingBottom: 60,
     
   },
