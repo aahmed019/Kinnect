1582da2086d7bc48f25b9f9a76ceb74f08a2c7c2
Lam Tran <ltran96@protonmail.com>
2020-10-08 18:26:21 -0400

    add winning and losing screen

--- src/components/gameRoom.js
+++ src/components/gameRoom.js
@@ -11,7 +11,7 @@ export default function GameRoom(props) {
   const [insideAQuestion, setInsideAQuestion] = useState(false) // check if the player inside the game or not (or if they are ready). If they are not inside the game, check if they won or lost =>  4 states: "inProgress", "Win", "Lose", false
   const [gameOver, setGameOver] = useState(false) // used to keep track when the game is over
   const [winner, setWinner] = useState(false) // answered correctly all the question
-  const [numberOfQuestions, setNumberOfQuestion] = useState(0) // number of question should be passed inside "props" when generate request
+  const [numberOfQuestions, setNumberOfQuestion] = useState(sampleGames.length) // number of question should be passed inside "props" when generate request
   const [nthQuestion, setNthQuestion] = useState(0) // keep track of which question the player is on (should be inside the database as well so once requested the player is moved to where it is currently is)
   const [currentAnswer, setCurrentAnswer] = useState(true) // will be used to decide if current question answered correctly
   // if currentAnswer is correct, a request for next question will be sent. Once the data received, "currentAnswer" set to False
@@ -35,8 +35,8 @@ export default function GameRoom(props) {
   } // used to get question data, "gameId, and nthQuestion" is received from the props
   const handleRightAnswer = () => {
     setCurrentAnswer(true)
-
-    nthQuestion == numberOfQuestions
+    console.log("currently at ", nthQuestion, "/", numberOfQuestions)
+    nthQuestion == numberOfQuestions - 1
       ? //if current question is last question => they won
       (
         setWinner(true),
@@ -72,12 +72,19 @@ export default function GameRoom(props) {
 
   }
   const tempHandleRightAnswer = () => {
-    (
-      setCurrentAnswer(true),
-      setNthQuestion(nthQuestion + 1),
-      console.log("Answer correctly, and now at question-nth: ", nthQuestion),
-      setQuestionData(sampleGames[nthQuestion + 1])
-    )
+    console.log("Answer correctly, and now at question-nth: ", nthQuestion, "/", numberOfQuestions),
+      (
+        nthQuestion == numberOfQuestions - 1
+          ? (setCurrentAnswer(false),
+            setInsideAQuestion(false),
+            setWinner(true),
+            setCurrentAnswer(true),
+            setNthQuestion(0))
+          : (setCurrentAnswer(true),
+            setNthQuestion(nthQuestion + 1),
+            setQuestionData(sampleGames[nthQuestion + 1])
+          )
+      )
   }
   // -------------------END OF HELPER FUNCTIONS-------------------
   // -------------------RENDERING CASES---------------------------
@@ -87,19 +94,19 @@ export default function GameRoom(props) {
   return (
     // <Button title="Exit" onPress={() => { setExit(true) }} />
     !insideAQuestion
-      ? !(winner && gameOver)
-        ? // if not Won or Lost the game => Just created 
+      ? winner
+        ? // if Won the game
         <View>
-          <Text onPress={() => tempGetQuestion()}>Click Here When Ready!</Text>
+          <Text>Congratulations!</Text>
         </View>
-        : winner // If the user won the game
+        : gameOver// If lose the game
           ?
           <View>
-            <Text>Congratulations!</Text>
-          </View>
-          : // If the user lost the game
-          <View>
             <Text>Sorry You Lost!</Text>
+          </View>
+          : // If room just created
+          <View>
+            <Text onPress={() => tempGetQuestion()}>Click Here When Ready!</Text>
           </View>
       : // If the user inside a question, see what type of question and render
       currentAnswer //if true that means waiting for next question
