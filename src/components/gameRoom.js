278bad3d9257e6538d3cc5ed6e7cc93ffebb515b
aahmed019 <42854126+aahmed019@users.noreply.github.com>
2020-11-08 18:17:40 -0500

    added ready state

--- src/components/gameRoom.js
+++ src/components/gameRoom.js
@@ -19,12 +19,14 @@ export default function GameRoom(props) {
   // if currentAnswer is correct, a request for next question will be sent. Once the data received, "currentAnswer" set to False
   const [questionData, setQuestionData] = useState({}) // everything needed to render a question
   const [questionType, setQuestionType] = useState('')
-  const [playerCount, setPlayerCount] = useState(0)
+  const [ready, setReady] = useState(0)
+  const [readied, setReadied] = useState(false)
   const[hostName, setHostname] = useState(props.hostname)
   const[hostid, setHostid] = useState(props.hostid)
   const [player, setPlayer] = useState(props.playerid)
   const [roomCode] = useState(props.gameID.toUpperCase())
   const[gameStatus, setGameStatus] = useState('lobby')
+  const [Timer, setTimer] = useState(3)
 
   
   //similar to componentDidMount & unMount
@@ -39,6 +41,11 @@ export default function GameRoom(props) {
       setNthQuestion(nthQuestion + 1)
    }
   }
+  //Listens to ready changes
+  Games.getRef(`games/${props.gameID}/ready`).on('value', (snapshot) =>{
+    let readyState = snapshot.val()
+    setReady(readyState)
+  });
   //Listens to round changes
   Games.getRef(`games/${props.gameID}/question`).on('value', (snapshot) => {
     let questState = snapshot.val();
@@ -102,7 +109,10 @@ export default function GameRoom(props) {
     setNthQuestion(0)
     alert('Sorry You Lost!')
   }
-
+const addPlayer = () =>{
+  Games.getRef(`games/${props.gameID}/ready`).set(firebase.database.ServerValue.increment(1))
+  setReadied(true)
+}
   // tempGetQuestion is for prototype and to read data from JSON file not from backend
   const tempGetQuestion = () => {
     Games.getRef(`games/${props.gameID}/status`).set('in-game');
@@ -117,11 +127,11 @@ export default function GameRoom(props) {
       :
       console.log("tempGetQuestion press")
 
-  }
+}
   const tempHandleRightAnswer = () => {
     console.log("Answer correctly, and now at question-nth: ", nthQuestion, "/", numberOfQuestions),
       (
-        nthQuestion == numberOfQuestions
+        nthQuestion == numberOfQuestions - 1
           ? (setCurrentAnswer(false),
             setInsideAQuestion(false),
             setWinner(true),
@@ -156,6 +166,18 @@ export default function GameRoom(props) {
   // False => Because of room just created or the game ended ?
   //Loading page
 
+  if(ready == 4 && Timer){
+    let interval = null;
+    interval = setInterval(() => {
+      setTimer(Timer => Timer - 1);
+    }, 1000);
+    if(Timer <= 0){
+    setTimer(false)
+    tempGetQuestion()
+    }
+  }
+
+  
   return (
     // <Button title="Exit" onPress={() => { setExit(true) }} />
     !insideAQuestion
@@ -170,12 +192,18 @@ export default function GameRoom(props) {
           ?
           <View >
             <Text style={styles.text}>Sorry You Lost!</Text>
-            <Text style={styles.text} onPress={() => exitGame()} style={styles.Text}>Back</Text>
+            <Text style={styles.text} onPress={() => exitGame()}>Back</Text>
           </View>
           : // If room just created
           <View>
-            <Text onPress={() => tempGetQuestion()} style={styles.text}>Click Here When Ready!</Text>
+            <Button onPress={() => addPlayer() } style={styles.text} disabled={readied} title = 'press'>Press here when ready!</Button>
+            
+            <Text style={styles.text}>{ready}/4</Text>
+            <Text style={styles.text}>{Timer}</Text>
+  <Text>{'\n\n\n\n'} </Text>
+  <Text style={styles.text} onPress ={() => {tempGetQuestion()}}>DEV</Text>
           </View>
+
       : // If the user inside a question, see what type of question and render
       currentAnswer //if true that means waiting for next question
         ? <View>
@@ -184,6 +212,7 @@ export default function GameRoom(props) {
         </View>
         :
         <View>
+          <Text style={styles.text} onPress={() => exitGame()}>Exit</Text>
           {
               nthQuestion == '0'
                ? <PictureQuestion
